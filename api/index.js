d9161f5f1e1f30e74b7863c49eddbc50a7005606
Julio Cesar Acosta <julio.acosta@coppel.com>
2024-05-03 16:02:13 -0700

    se cambia la cascada de inicio de server

--- api/index.js
+++ api/index.js
@@ -5,32 +5,32 @@ const { typeDefs } = require('./models/typeDefs.js');
 const  { InMemoryLRUCache } = require('@apollo/utils.keyvaluecache');
 
 const MONGO_URI = "mongodb+srv://culiacan:1234-@cluster0.2xgtfpu.mongodb.net/prueba?retryWrites=true&w=majority&appName=Cluster0";
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log(`Db Connected`);
-    const server = new ApolloServer({
-      typeDefs,
-      resolvers,
-      context: async (req, res) => ({ req, res }),
-      cache: new InMemoryLRUCache({
-        // ~100MiB
-        maxSize: Math.pow(2, 20) * 100,
-        // 5 minutes (in seconds)
-        ttl: 300,
-      }),
-    });
-    
-    server
-    .listen({port: process.env.PORT || 4000})
-    .then(({url}) => {
-        console.log(`corriendo ${url}`)
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async (req, res) => ({ req, res }),
+  cache: new InMemoryLRUCache({
+    // ~100MiB
+    maxSize: Math.pow(2, 20) * 100,
+    // 5 minutes (in seconds)
+    ttl: 300,
+  }),
+});
+
+server
+.listen({port: process.env.PORT || 4000})
+.then(({url}) => {
+    console.log(`corriendo ${url}`)
+    mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+      console.log(`Db Connected`);
     })
-  })
-  .catch(err => {
-    console.log(err);
-    console.log(err.message);
-  });
+    .catch(err => {
+      console.log(err);
+      console.log(err.message);
+    });
+})
 
 
 //const handler = startServerAndCreateNextHandler(server, {
