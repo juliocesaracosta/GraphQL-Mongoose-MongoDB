a9177ee6d418561f2895eda327d54be1e9cd5f00
Julio Cesar Acosta <julio.acosta@coppel.com>
2024-05-03 14:39:37 -0700

    se remueve el cors y se agrega el vercel.json

---
+++ api/index.js
@@ -0,0 +1,41 @@
+const { ApolloServer} = require ('apollo-server');
+const { resolvers } = require('./resolvers.js');
+const mongoose = require("mongoose");
+const { typeDefs } = require('./models/typeDefs.js');
+const  { InMemoryLRUCache } = require('@apollo/utils.keyvaluecache');
+
+const MONGO_URI = "mongodb+srv://culiacan:1234-@cluster0.2xgtfpu.mongodb.net/prueba?retryWrites=true&w=majority&appName=Cluster0";
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log(`Db Connected`);
+  })
+  .catch(err => {
+    console.log(err);
+    console.log(err.message);
+  });
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async (req, res) => ({ req, res }),
+  cache: new InMemoryLRUCache({
+    // ~100MiB
+    maxSize: Math.pow(2, 20) * 100,
+    // 5 minutes (in seconds)
+    ttl: 300,
+  }),
+});
+
+server
+.listen({port: process.env.PORT || 4000})
+.then(({url}) => {
+    console.log(`corriendo ${url}`)
+})
+
+//const handler = startServerAndCreateNextHandler(server, {
+//  context: async (req, res) => ({ req, res }),
+//})
+
+//export default allowCors(handler)
